91622da1df65c2b1cd2f5b17f59a489b132017d4
Edmundo Sanchez <zomundo@gmail.com>
2018-08-28 03:37:44 -0500

    awis cli to query

--- lib/utils.rb
+++ lib/utils.rb
@@ -4,14 +4,14 @@ module Aws
     # Reads given CSV, gets domain from last column
     # @param path [String] path to the csv
     # @return Array
-    def readCSV(path)
+    def self.readCSV(path)
       data = CSV.read(path)
       return data
     end
     # Reads given CSV, gets domain from last column
     # @param path [String] path to the csv
     # @param data [Array] Array of arrays containing information for the csv file
-    def writeCSV(path,data)
+    def self.writeCSV(path,data)
       CSV.open("myfile.csv", "w") do |csv|
         data.each do |row|
           csv << row
