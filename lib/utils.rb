0d6dbc26c4e4503ee7232d53955a525dfcc442d5
Edmundo Sanchez <zomundo@gmail.com>
2018-08-28 02:07:44 -0500

    read and writte to a csv file

---
+++ lib/utils.rb
@@ -0,0 +1,22 @@
+module Aws
+  require "csv"
+  class Utils
+    # Reads given CSV, gets domain from last column
+    # @param path [String] path to the csv
+    # @return Array
+    def readCSV(path)
+      data = CSV.read(path)
+      return data
+    end
+    # Reads given CSV, gets domain from last column
+    # @param path [String] path to the csv
+    # @param data [Array] Array of arrays containing information for the csv file
+    def writeCSV(path,data)
+      CSV.open("myfile.csv", "w") do |csv|
+        data.each do |row|
+          csv << row
+        end
+      end
+    end
+  end
+end
