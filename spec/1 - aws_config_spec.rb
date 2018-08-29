9280b30998c5aa7dbfd43f21d8624e3443d31680
Edmundo Sanchez <zomundo@gmail.com>
2018-08-28 20:35:31 -0500

    specs fail due to lingering env variable, clean it up

--- spec/1 - aws_config_spec.rb
+++ spec/1 - aws_config_spec.rb
@@ -7,10 +7,12 @@ RSpec.describe Aws::AWS_config do
     ENV["AWS"] = "#{ENV["HOME"]}/.aws"
     file = Aws::AWS_config.new
     expect(file.read_keys).to include(:aws_access_key_id, :aws_secret_access_key)
+    ENV.delete("AWS") #clean up ENV["AWS"]
   end
   it "fails if file is not found" do
     ENV["AWS"] = "wrong_location"
     file = Aws::AWS_config.new
     expect {file.read_keys}.to raise_error Errno::ENOENT
+    ENV.delete("AWS") #clean up ENV["AWS"]
   end
 end
